4a9b4078949bd89dd6c56d84294218afbc30e2cd
AndresRojas22 <andy_chapa01@hotmail.com>
2021-11-16 14:36:24 -0600

    Creacion de acceso a Datos y Entidades

---
+++ Proyecto-Ajedriux/Entidades/EntidadHotel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class EntidadHotel
+    {
+        public int _Id_Hotel { get; set; }
+        public string _Nombre { get; set; }
+        public string _Telefono { get; set; }
+        public string _Direccion { get; set; }
+        public EntidadHotel(int id_Hotel, string Nombre,
+            string Telefono, string Direccion)
+        {
+            _Id_Hotel = id_Hotel;
+            _Nombre = Nombre;
+            _Telefono = Telefono;
+            _Direccion = Direccion;
+        }
+        public EntidadHotel()
+        {
+
+        }
+    }
+}
